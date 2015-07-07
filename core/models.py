666dc4fed3e687f66e941a7fe741ac18510198d7
paulee7 <7.paul.lee@gmail.com>
2015-07-06 20:06:02 -0600

    pretty sweet day!

--- core/models.py
+++ core/models.py
@@ -1,3 +1,32 @@
 from django.db import models
-
+from django.core.urlresolvers import reverse
 # Create your models here.
+
+import os
+import uuid
+
+# tell django where to save the incoming file and what to name it
+# this is a piece of code that works really well with S3.
+def upload_to_location(instance, filename):
+    blocks = filename.split('.')
+    ext = blocks[-1]
+    filename = "%s.%s" % (uuid.uuid4(), ext)
+    instance.title = blocks[0]
+    return os.path.join('uploads/', filename)
+
+
+class Location(models.Model):
+	title = models.CharField(max_length = 300)
+	description = models.TextField(null=True, blank=True)
+	created_at = models.DateTimeField(auto_now_add=True)
+	address = models.TextField(null=True, blank=True)
+	hours = models.TextField(null=True, blank=True)
+	image_file = models.ImageField(upload_to=upload_to_location, null=True, blank=True)
+
+
+
+	def __unicode__(self):
+		return self.title
+
+	def get_absolute_url(self):
+		return reverse(viewname="location_list", args=[self.id])
--- core/urls.py
+++ core/urls.py
@@ -14,10 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
 from django.conf.urls import patterns, include, url
+from django.contrib import admin
 import core.views as coreviews
 
 
 urlpatterns = patterns('',
 
     url(r'^$', coreviews.LandingView.as_view()),
+    url(r'location/$', coreviews.LocationListView.as_view()),
+	url(r'location/(?P<pk>\d+)/detail/$', coreviews.LocationDetailView.as_view(), name='location_list'),
+
 )
